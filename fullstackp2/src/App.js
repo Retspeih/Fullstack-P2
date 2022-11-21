a4af1ff3dfe181bfe9da3e438e5bb938a9d997e1
Retspeih <hlnguyen504@gmail.com>
2022-11-20 19:03:40 -0500

    table and buttons have been formatted, still needs polishing

--- fullstackp2/src/App.js
+++ fullstackp2/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
 import data from './table-data.json'
+import Form from 'react-bootstrap/Form';
 import './App.css'
 
 function App() {
@@ -16,13 +18,41 @@ function App() {
                 </p>
             </div>
             <Table striped bordered hover>
+                <thead>
+                    <tr class="centered-text">
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Deadline</th>
+                        <th>Priority</th>
+                        <th>Is Complete</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
                 <tbody>
                     {info.map((information) => (
-                        <tr>
+                        <tr class="centered-text">
                             <td>{information.id}</td>
                             <td>{information.firstName}</td>
                             <td>{information.lastName}</td>
                             <td>{information.username}</td>
+                            <td>
+                                <Form>
+                                    {['checkbox'].map((type) => (
+                                        <div key={`default-${type}`} className="mb-3">
+                                            <Form.Check
+                                                type={type}
+                                                id={`default-${type}`}
+                                            />
+                                        </div>
+                                    ))}
+                                </Form>
+                            </td>
+                            <td>
+                                <Stack>
+                                    <Button>action</Button>
+                                    <Button>delete</Button>
+                                </Stack>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
